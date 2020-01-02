d551f6318f8a90deae727d985c10b58da04d73cf
Sandeep Sachdev <sandeepsachdev17@gmail.com>
2020-01-02 16:08:19 +1100

    Get working in heroku

--- src/main/java/com/eduonix/projectbackend/security/AuthServerConfig.java
+++ src/main/java/com/eduonix/projectbackend/security/AuthServerConfig.java
@@ -43,7 +43,7 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 			HttpServletResponse res = (HttpServletResponse) response;
 
 
-			res.setHeader("Access-Control-Allow-Origin", "https://sandeepsachdev.github.io/");
+			res.setHeader("Access-Control-Allow-Origin", "*");
 			res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
 			res.setHeader("Access-Control-Max-Age", "3600");
 			res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
