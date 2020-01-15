2e83512006065140375b3003bd18300517c365a1
sandeepsachdev <sandeepsachdev17@gmail.com>
2020-01-15 20:15:24 +1100

    Clear cache every 10 seconds

--- src/main/java/com/eduonix/projectbackend/cache/CachingManager.java
+++ src/main/java/com/eduonix/projectbackend/cache/CachingManager.java
@@ -1,17 +1,29 @@
 package com.eduonix.projectbackend.cache;
 
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.Date;
 
 @Configuration
 @EnableCaching
+@EnableScheduling
 public class CachingManager {
 
     @Bean
     public CacheManager cacheManager() {
         return new ConcurrentMapCacheManager("tweets");
+    }
+
+    @CacheEvict(allEntries = true, value = {"tweets"})
+    @Scheduled(fixedDelay = 600  * 1000 ,  initialDelay = 500)
+    public void reportCacheEvict() {
+        System.out.println("Flush Cache ");
     }
 }
