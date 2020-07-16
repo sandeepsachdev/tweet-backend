0aba614a1be9940557633aa06f524bac3dbb3ed3
Sandeep Sachdev <sandeepsachdev17@gmail.com>
2020-07-16 17:16:30 +1000

    rss service

--- src/main/java/com/eduonix/projectbackend/controller/ApiController.java
+++ src/main/java/com/eduonix/projectbackend/controller/ApiController.java
@@ -1,7 +1,9 @@
 package com.eduonix.projectbackend.controller;
 
 
+import com.apptastic.rssreader.Item;
 import com.eduonix.projectbackend.model.Tweet;
+import com.eduonix.projectbackend.service.SmhRssService;
 import com.eduonix.projectbackend.service.TweetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +17,19 @@ public class ApiController {
 	@Autowired
 	TweetService tweetService;
 
+	@Autowired
+	SmhRssService smhRssService;
+
 	@RequestMapping(value="/getTweets",produces="application/json")
 	public List<Tweet> getTweets() {
 		return tweetService.getTweets();
 	}
-	
-	
+
+	@RequestMapping(value="/getSmhRss",produces="application/json")
+	public List<Item> getSmhRss() {
+		return smhRssService.getRss();
+	}
+
+
 
 }
---
+++ src/main/java/com/eduonix/projectbackend/service/SmhRssService.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2012-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.eduonix.projectbackend.service;
+
+import com.apptastic.rssreader.Item;
+import com.apptastic.rssreader.RssReader;
+import com.eduonix.projectbackend.model.Tweet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.lang.System.exit;
+
+@Component
+public class SmhRssService {
+
+    private static final Map<String, Item> articlesMap = new HashMap<String, Item>();
+    private static final Logger log = LoggerFactory.getLogger(SmhRssService.class);
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    public List<Item> getRss() {
+        Collection<Item> items = articlesMap.values();
+        List<Item> itemList = new ArrayList<>(items);
+        itemList.sort(Comparator.comparing(o -> o.getPubDateZonedDateTime().get()));
+        itemList.sort(Comparator.reverseOrder());
+        return itemList;
+    }
+
+    @Scheduled(fixedRate = 3600000)
+    private void printSmhFeed() {
+
+        RssReader reader = new RssReader();
+        Stream<Item> rssFeed = null;
+        List<Item> articles = new ArrayList<>();
+
+        {
+            try {
+                articles.addAll(reader.read("https://www.smh.com.au/rss/feed.xml").collect(Collectors.toList()));
+                articles.addAll(reader.read("https://www.smh.com.au/rss/politics/federal.xml").collect(Collectors.toList()));
+                articles.addAll(reader.read("https://www.smh.com.au/rss/national/nsw.xml").collect(Collectors.toList()));
+                articles.addAll(reader.read("https://www.smh.com.au/rss/world.xml").collect(Collectors.toList()));
+                articles.addAll(reader.read("https://www.smh.com.au/rss/national.xml").collect(Collectors.toList()));
+                articles.addAll(reader.read("https://www.smh.com.au/rss/business.xml").collect(Collectors.toList()));
+                articles.addAll(reader.read("https://www.smh.com.au/rss/culture.xml").collect(Collectors.toList()));
+                articles.addAll(reader.read("https://www.smh.com.au/rss/lifestyle.xml").collect(Collectors.toList()));
+            } catch (IOException e) {
+                e.printStackTrace();
+                exit(0);
+            }
+        }
+
+        for (Item item : articles) {
+            if (item.getGuid().isPresent() && !articlesMap.containsKey(item.getGuid().get())) {
+                articlesMap.put(item.getGuid().get(), item);
+                System.out.println(item.getGuid().get() + " " + item.getLink().get());
+            }
+        }
+
+        log.info("The time is now {}", dateFormat.format(new Date()));
+    }
+
+}
