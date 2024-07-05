d007a431ff65334e608d00460bdc46e721d1fd12
sandeepsachdev <sandeepsachdev17@gmail.com>
2024-07-05 21:37:39 +1000

    trends

--- src/main/java/com/eduonix/projectbackend/controller/ApiController.java
+++ src/main/java/com/eduonix/projectbackend/controller/ApiController.java
@@ -1,15 +1,10 @@
 package com.eduonix.projectbackend.controller;
 
 
-import com.apptastic.rssreader.Item;
 import com.eduonix.projectbackend.model.*;
 import com.eduonix.projectbackend.service.SmhRssService;
-import com.eduonix.projectbackend.service.TweetService;
-import com.eduonix.projectbackend.service.TweetTrendService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -23,26 +18,10 @@ public class ApiController {
 	String newsApiKey;
 
 	@Autowired
-	TweetService tweetService;
-
-	@Autowired
 	SmhRssService smhRssService;
 
 	@Autowired
 	RestTemplate restTemplate;
-
-	@Autowired
-	TweetTrendService tweetTrendService;
-
-	@RequestMapping(value="/getTweets",produces="application/json")
-	public List<Tweet> getTweets() {
-		return tweetService.getTweets();
-	}
-
-	@RequestMapping(value="/getTweetTrends",produces="application/json")
-	public List<TweetTrend> getTweetTrends() {
-		return tweetTrendService.getTweetTrends();
-	}
 
 	@RequestMapping(value="/getSmhRss",produces="application/json")
 	public List<SmhItem> getSmhRss() {
